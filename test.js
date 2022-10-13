7a3e09e6cac3f7b05d6f1b44c9557e381ff50130
Deepin Developer <deepin-dev@deepin.org>
2022-10-13 17:03:44 +0800

    feat: Init commit
    init node-invert-kv
    
    Log:

---
+++ test.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+import invertKeyValue from '.';
+
+test('throws an error if wrong type is given', t => {
+	t.throws(
+		() => {
+			invertKeyValue(null);
+		},
+		{
+			instanceOf: TypeError,
+			message: 'Expected an object'
+		}
+	);
+});
+
+test('inverts string/number properties', t => {
+	t.deepEqual(
+		invertKeyValue({foo: 'bar', unicorn: 'rainbow', 1: 'one'}),
+		{bar: 'foo', rainbow: 'unicorn', one: '1'}
+	);
+});
+
+test('inverts symbol properties', t => {
+	const symbol = Symbol('fixture');
+	t.deepEqual(
+		invertKeyValue({foo: symbol, unicorn: 'rainbow'}),
+		{[symbol]: 'foo', rainbow: 'unicorn'}
+	);
+	t.deepEqual(
+		invertKeyValue({[symbol]: 'foo', rainbow: 'unicorn'}),
+		{foo: symbol, unicorn: 'rainbow'}
+	);
+});
